3451d4feaf78fcfb4db3805f9b405dac46c25508
Leonardo Moreno <garrik.leonardo@gmail.com>
2016-02-07 15:44:19 -0500

    UserApi service

---
+++ src/app/components/userApi/userApi.service.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('testBitgray')
+    .factory('userApi', userApi);
+
+  /** @ngInject */
+  function userApi() {
+
+    var service = {
+    };
+
+    return service;
+
+  }
+})();
